32ebedc91b2b00c1e1d6eaebf69be524acee4047
dinet6954 <87867800+dinet6954@users.noreply.github.com>
2025-08-16 08:25:23 +0530

    Fixed enemy AI not respawning in rewind, fixed enemy gun drop on death

--- Game Jam Loop/Assets/Scripts/Game Mechanic Scripts/Enemy/Target.cs
+++ Game Jam Loop/Assets/Scripts/Game Mechanic Scripts/Enemy/Target.cs
@@ -28,7 +28,7 @@ public class Target : MonoBehaviour
     void Awake()
     {
         Player = GameObject.Find("FirstPersonController");
-        TimeRewind timeRewind = Player.GetComponent<TimeRewind>();
+        TimeRewind = Player.GetComponent<TimeRewind>();
         EnemyAI EAI = GetComponent<EnemyAI>();
         health = new List<float>();
         Enemy = this.gameObject;
@@ -114,6 +114,8 @@ public class Target : MonoBehaviour
         TimeSinceDeath = 0;
         Destroy(DroppedGun);
         EAI.SetDamage();
+        dropped = false;
+        once = true;
     }
 
     void Record()
